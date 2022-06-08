990699f6a9370e98310e1ae7a7e2a4c9aef495a7
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-08 09:40:56 +0500

    #23

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -71,11 +71,16 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://boilerplate-mochachai.shoxruhr.repl.co'
 
 suite('Functional Tests with Zombie.js', function () {
+  const browser = new Browser
+
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
+
   this.timeout(5000);
-
-
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function () {
